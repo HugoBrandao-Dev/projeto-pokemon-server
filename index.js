90a605bd140b3e43dc041c0beef8cd51b95929ce
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2023-03-15 15:43:49 -0400

    Testada conexao com o banco de dados, inserindo dados ficticios no mesmo.

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const express = require('express')
 const app = express()
+const DATABASE = require('./database/connection.js')
 
-app.get("/", (req, res) => {
-  res.send('<h1>Home</h1>')
+app.get("/login", (req, res) => {
+  DATABASE.insert({
+    full_name: 'Tobias de Oliveira',
+    born_date: '2023-03-15',
+    email: 'tobias@gmail.com',
+    user_password: 'tobias123'
+  }).into('users')
+    .then(response => {
+      res.send('Usuário inserido com sucesso.')
+    })
+    .catch(error => {
+      console.log(error)
+    })
 })
 
 app.listen(4000, error => {
