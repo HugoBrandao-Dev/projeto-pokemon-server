bae3400c2e1c553f4c107d2023d0f1f8ab9d9c48
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2023-03-15 12:37:25 -0400

    Criado script para a criacao da tabela de pokemons capturados.

---
+++ sql_scripts/captured_pokemons.sql
@@ -0,0 +1,21 @@
+/*
+Essa tabela não guardará todos os pokemons da geração configurada no front-end, mas sim os pokemons 
+já capturados por algum usuário.
+*/
+
+CREATE TABLE captured_pokemons (
+	id INT NOT NULL AUTO_INCREMENT,
+	chain_id INT NOT NULL,
+	/*
+	evolution_id 1 = forma base;
+	evolution_id 2 = primeira evolução;
+	evolution_id 3 = segunda evolução.
+	*/
+	evolution_id INT NOT NULL,
+	# Diferença entre a experiência que ganhou com lutas e a base_experience.
+	experience_plus INT NOT NULL,
+	PRIMARY KEY (id)
+);
+
+DESCRIBE captured_pokemons;
+DROP TABLE captured_pokemons;
