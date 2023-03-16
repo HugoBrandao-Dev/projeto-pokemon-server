49aa79a4f2f6e92db01e9253933aaf8cbbe3b41a
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2023-03-16 19:24:02 -0400

    Modificadas as informacoes que serao armazenada no banco de dados, para que armazene o nome do item e tambem se tipo. Adicionado comando de selecao geral dos items ja cadastrados.

--- sql_scripts/items.sql
+++ sql_scripts/items.sql
@@ -1,12 +1,14 @@
 CREATE TABLE items (
 	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-	# O ID do item (item_id) deve ser proveniente da PokeAPI.
-	item_id INT UNSIGNED NOT NULL,
+	item VARCHAR(50) NOT NULL,
+	type_id INT UNSIGNED NOT NULL,
 	consumable BOOLEAN NOT NULL DEFAULT 0,
 	cumulative BOOLEAN NOT NULL DEFAULT 0,
 	quest_item BOOLEAN NOT NULL DEFAULT 0,
-	PRIMARY KEY (id)
+	PRIMARY KEY (id),
+	CONSTRAINT fk_type_id FOREIGN KEY (type_id) REFERENCES items_types(id)
 );
 
+SELECT * FROM items;
 DESCRIBE items;
 DROP TABLE items;
