1e4ca6b40bc15a607453352c5d5cc45301bb20c4
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2023-03-16 18:16:48 -0400

    Criado script de criacao da tabela de relacionamento dos pokemons e usuarios.

---
+++ sql_scripts/users_pokemons.sql
@@ -0,0 +1,9 @@
+CREATE TABLE users_pokemons (
+	user_id INT UNSIGNED NOT NULL,
+	pokemon_id INT UNSIGNED NOT NULL,
+	CONSTRAINT fk_users_id FOREIGN KEY (user_id) REFERENCES users(id),
+	CONSTRAINT fk_pokemon_id FOREIGN KEY (pokemon_id) REFERENCES captured_pokemons(id)
+);
+
+SELECT * FROM users_pokemons;
+DESCRIBE users_pokemons;
