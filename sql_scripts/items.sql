24d6379a76cac02de17adee95194ad97d9edd25a
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2023-03-17 12:25:19 -0400

    Implementado codigo dos registros dos itens de pokebolas e frutas.

--- sql_scripts/items.sql
+++ sql_scripts/items.sql
@@ -9,6 +9,19 @@ CREATE TABLE items (
 	CONSTRAINT fk_type_id FOREIGN KEY (type_id) REFERENCES items_types(id)
 );
 
+# Insere os registros das poke-bolas.
+INSERT INTO items (item, type_id, consumable, cumulative) VALUES
+("poke-ball", 1, 1, 1),
+("great-ball", 1, 1, 1),
+("ultra-ball", 1, 1, 1),
+("master-ball", 1, 1, 1);
+
+# Insere os registros das frutas.
+INSERT INTO items (item, type_id, consumable, cumulative) VALUES
+("jaboca-berry", 2, 1, 1),
+("razz-berry", 2, 1, 1),
+("bluk-berry", 2, 1, 1);
+
 SELECT * FROM items;
 DESCRIBE items;
 DROP TABLE items;
