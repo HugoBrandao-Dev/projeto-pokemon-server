653634985083932ca1ecfb5c75e2c73fae963fb2
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2023-03-27 23:11:56 -0400

    Modificado codigo de autenticacao, para que se verifique o token no header e nao mais a sessao do usuario.

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,5 +1,23 @@
+const jwt = require('jsonwebtoken')
+
+const secret = 'qwuirlsklaflks23'
+
 function authentication(req, res, next) {
-  if (req.session.user) {
+  const authToken = req.headers['authorization']
+
+  if (authToken) {
+    const bearer = authToken.split(' ')
+    const token = bearer[1]
+    try {
+      let decoded = jwt.verify(token, secret)
+
+      if (decoded) {
+        next()
+      }
+    } catch (error) {
+      console.error(error)
+      res.send('Token inválido.')
+    }
     next()
   } else {
     res.send('Faça o login.')
