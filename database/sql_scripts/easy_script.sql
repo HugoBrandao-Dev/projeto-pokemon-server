dee38cea217aa264550b839bdc36704d993071d2
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2023-04-07 21:47:21 -0400

    Implementado script que facilita a limpeza dos registro de algumas tabelas.

---
+++ database/sql_scripts/easy_script.sql
@@ -0,0 +1,16 @@
+USE pokemon_project;
+
+#SELECT * FROM users;
+TRUNCATE TABLE users;
+
+#SELECT * FROM users_pokemons;
+TRUNCATE TABLE users_pokemons;
+
+#SELECT * FROM captured_pokemons;
+TRUNCATE TABLE captured_pokemons;
+
+#SELECT * FROM users_pokemons;
+TRUNCATE TABLE users_pokemons;
+
+#SELECT * FROM users_items;
+TRUNCATE TABLE users_items;
