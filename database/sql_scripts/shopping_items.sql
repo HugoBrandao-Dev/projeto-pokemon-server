853249353b2c81cc2165d063dbd8373494783923
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2023-04-08 22:04:20 -0400

    Criado script de criacao da tabela responsavel por armazenar os itens do shopping e seus precos.

---
+++ database/sql_scripts/shopping_items.sql
@@ -0,0 +1,26 @@
+USE pokemon_project;
+
+CREATE TABLE shopping_items (
+	item_id INT UNSIGNED NOT NULL,
+	item_price_id INT UNSIGNED NOT NULL,
+	amount INT UNSIGNED NOT NULL,
+	CONSTRAINT fk_item_id FOREIGN KEY (item_id) REFERENCES items(id),
+	CONSTRAINT fk_item_price_id FOREIGN KEY (item_price_id) REFERENCES items(id)
+);
+
+INSERT INTO shopping_items VALUES 
+(1, 8, 20),
+(2, 8, 50),
+(3, 9, 10),
+(4, 10, 2),
+(5, 8, 5),
+(6, 9, 1),
+(7, 10, 1);
+
+SELECT * FROM shopping_items;
+DESCRIBE shopping_items;
+
+# Comando para excluir todos os registros da tabela, sem a necessidade de excluir a tabela.
+TRUNCATE TABLE shopping_items;
+
+DROP TABLE shopping_items;
