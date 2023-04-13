43be6c43e42cbdf31bba30cdc5cff92deee06ee6
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2023-04-12 23:44:00 -0400

    Adicionados comandos para insercao de dados TESTES na tabela de pokemons capturados e na tabela de relacionamento pokemons/users, alem de outros comandos.

--- database/sql_scripts/easy_script.sql
+++ database/sql_scripts/easy_script.sql
@@ -1,16 +1,39 @@
 USE pokemon_project;
 
 #SELECT * FROM users;
+#DROP TABLE users;
 TRUNCATE TABLE users;
 
 #SELECT * FROM users_pokemons;
+#DROP TABLE users_pokemons;
 TRUNCATE TABLE users_pokemons;
 
 #SELECT * FROM captured_pokemons;
+#DROP TABLE captured_pokemons;
 TRUNCATE TABLE captured_pokemons;
-
-#SELECT * FROM users_pokemons;
-TRUNCATE TABLE users_pokemons;
 
 #SELECT * FROM users_items;
 TRUNCATE TABLE users_items;
+
+INSERT INTO captured_pokemons(
+	specie,
+	chain_id,
+	evolution_id,
+	experience_plus,
+	ball_id,
+	battles,
+	battles_won) VALUES 
+('bulbasaur', 1, 1, 0, 1, 0, 0),
+('pichu', 1, 1, 0, 1, 0, 0),
+('charmander', 1, 1, 0, 1, 0, 0),
+('squertle', 1, 1, 0, 1, 0, 0);
+
+INSERT INTO users_pokemons(user_id, pokemon_id) VALUES 
+(1, 1),
+(1, 2),
+(1, 3),
+(1, 4),
+(2, 5),
+(2, 6),
+(2, 7),
+(2, 8);
